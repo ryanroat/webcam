72c607580dfd5f370b2ec340f2d99256333c9d94
Ryan Roat <ryanroat@gmail.com>
2018-02-12 21:09:24 -0500

    adding photo options/filters

--- js/main.js
+++ js/main.js
@@ -7,6 +7,10 @@ let width = 500,
 // DOM elements
 const video = document.getElementById('video');
 const canvas = document.getElementById('canvas');
+const photos = document.getElementById('photos');
+const photoButton = document.getElementById('photo-button');
+const clearButton = document.getElementById('clear-button');
+const photoFilter = document.getElementById('photo-filter');
 
 // Get media stream
 navigator.mediaDevices.getUserMedia({video: true, audio: false})
