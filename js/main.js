17c9a3d5e03f8fc0ed189dfa0e6c812eadab5dca
Ryan Roat <ryanroat@gmail.com>
2018-02-27 20:41:26 -0500

    added capture image onto canvas

--- js/main.js
+++ js/main.js
@@ -16,10 +16,43 @@ const photoFilter = document.getElementById('photo-filter');
 navigator.mediaDevices.getUserMedia({video: true, audio: false})
   .then(function(stream) {
     //link to the video source
-    video.srcObject =stream;
+    video.srcObject = stream;
     // Play video
     video.play();
   })
   .catch(function(err) {
     console.log(`Error: ${err}`);
   }); 
+
+  // Play when ready
+
+video.addEventListener('canplay', function(e) {
+  if(!streaming) {
+    // Set video / canvas height
+    height = video.videoHeight / (video.videoWidth / width);
+
+    video.setAttribute('width', width);
+    video.setAttribute('height', height);
+    canvas.setAttribute('width', width);
+    canvas.setAttribute('height', height);
+
+    streaming = true;
+  }
+}, false);
+
+photoButton.addEventListener('click', function(e){
+  takePicture();
+  e.preventDefault();
+}, false);
+
+function takePicture(e) {
+  // Create canvas
+  const context = canvas.getContext('2d');
+  if(width && height) {
+    // set canvas properties
+    canvas.width = width;
+    canvas.height = height;
+    // Draw an image of the video on the canvas
+    context.drawImage(video, 0, 0, width, height);
+  }
+};
