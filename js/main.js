32309c1fc7289adb8b3749bdfb3c02d938e2eaa5
Ryan Roat <ryanroat@gmail.com>
2020-07-24 13:54:23 -0400

    prettier code formatting

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 // global variables
-let width = 750,
-    height = 0,
-    filter = 'none',
-    streaming = false;
+const width = 750;
+let height = 0;
+let filter = 'none';
+let streaming = false;
 
 // DOM elements
 const video = document.getElementById('video');
@@ -13,86 +13,92 @@ const clearButton = document.getElementById('clear-button');
 const photoFilter = document.getElementById('photo-filter');
 
 // Get media stream
-navigator.mediaDevices.getUserMedia({video: true, audio: false})
-  .then(function(stream) {
-    //link to the video source
-    video.srcObject = stream;
-    // Play video
-    video.play();
-  })
-  .catch(function(err) {
-    console.log(`Error: ${err}`);
-  }); 
+navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    .then(function(stream) {
+        // link to the video source
+        video.srcObject = stream;
+        // Play video
+        video.play();
+    })
+    .catch(function(err) {
+        console.log(`Error: ${err}`);
+    });
 
-  // Play when ready
+// Play when ready
 
-video.addEventListener('canplay', function(e) {
-  if(!streaming) {
-    // Set video / canvas height
-    height = video.videoHeight / (video.videoWidth / width);
+video.addEventListener(
+    'canplay',
+    function(e) {
+        if (!streaming) {
+            // Set video / canvas height
+            height = video.videoHeight / (video.videoWidth / width);
 
-    video.setAttribute('width', width);
-    video.setAttribute('height', height);
-    canvas.setAttribute('width', width);
-    canvas.setAttribute('height', height);
+            video.setAttribute('width', width);
+            video.setAttribute('height', height);
+            canvas.setAttribute('width', width);
+            canvas.setAttribute('height', height);
 
-    streaming = true;
-  }
-}, false);
-
-
+            streaming = true;
+        }
+    },
+    false
+);
 
 // Photo button event
-photoButton.addEventListener('click', function(e){
-  takePicture();
-  e.preventDefault();
-}, false);
+photoButton.addEventListener(
+    'click',
+    function(e) {
+        takePicture();
+        e.preventDefault();
+    },
+    false
+);
 
 // Filter event
 photoFilter.addEventListener('change', function(e) {
-  e.preventDefault();
-  // Set filter to chosen option
-  filter = e.target.value;
-  // Set filter option to video
-  video.style.filter = filter;
+    e.preventDefault();
+    // Set filter to chosen option
+    filter = e.target.value;
+    // Set filter option to video
+    video.style.filter = filter;
 });
 
 // Clear event
 clearButton.addEventListener('click', function(e) {
-  e.preventDefault();
-  // Clear all photos
-  photos.innerHTML = '';
-  // Clear filters
-  filter = 'none';
-  video.style.filter = filter;
-  photoFilter.selectedIndex = 0;
-
-})
+    e.preventDefault();
+    // Clear all photos
+    photos.innerHTML = '';
+    // Clear filters
+    filter = 'none';
+    video.style.filter = filter;
+    photoFilter.selectedIndex = 0;
+});
 
 // Take picture from canvas
 function takePicture(e) {
-  // Create canvas
-  const context = canvas.getContext('2d');
-  if(width && height) {
-    // set canvas properties
-    canvas.width = width;
-    canvas.height = height;
-    // Draw an image of the video on the canvas
-    context.drawImage(video, 0, 0, width, height);
+    // Create canvas
+    const context = canvas.getContext('2d');
+    if (width && height) {
+        // set canvas properties
+        canvas.width = width;
+        canvas.height = height;
+        // Draw an image of the video on the canvas
+        context.drawImage(video, 0, 0, width, height);
 
-    // Create image from canvas
-    const imgUrl = canvas.toDataURL('image/png');
+        // Create image from canvas
+        const imgUrl = canvas.toDataURL('image/png');
 
-    // Create image element
-    const img = document.createElement('img');
+        // Create image element
+        const img = document.createElement('img');
 
-    // Set image source
-    img.setAttribute('src', imgUrl);
+        // Set image source
+        img.setAttribute('src', imgUrl);
 
-    // Set image filter option
-    img.style.filter = filter;
+        // Set image filter option
+        img.style.filter = filter;
 
-    //append image to #photos 
-    photos.appendChild(img);
-  }
-};
+        // append image to #photos
+        photos.appendChild(img);
+    }
+}
